4b6c5a376c5fdeaae9045a4ca94fab130db095bf
liamSully43 <liamsullivan43@gmail.com>
2021-04-25 14:36:41 +0100

    Initial Commit
    First Commit of the Profile Page.
    This includes:
     - Content
     - Styling of content
     - Animated background
     - Links toggle
     - Image delay
     - Responsive design

---
+++ script.js
@@ -0,0 +1,135 @@
+const linksButton = document.querySelector(".svg-container");
+
+linksButton.onclick = toggleLinks;
+
+function toggleLinks() {
+    const shown = document.querySelector(".show");
+    if(shown) {
+        // hide links
+        document.querySelector(".links-container").classList.remove("show"); 
+        document.querySelector(".links-container").classList.add("hide"); 
+    }
+    else {
+        // show links
+        document.querySelector(".links-container").classList.add("show");
+        document.querySelector(".links-container").classList.remove("hide");
+    }
+}
+
+setTimeout(() => {
+    document.querySelector(".profile").classList.add("fade");
+}, 1200);
+
+// Config for the animated backgrounds 
+
+const particlesConfig = {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 2,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 5,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": true,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "window",
+      "events": {
+        "onhover": {
+          "enable": false
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": false
+  }
+  
+particlesJS("particles-js", particlesConfig);
